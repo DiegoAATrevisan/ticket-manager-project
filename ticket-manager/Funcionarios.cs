c9e179760aff8efd696232730faff2404d04bf89
Diego A A Trevisan <augustodiego02@gmail.com>
2026-06-15 16:27:13 -0300

    Inicializado implementação da classe de funcionários.

---
+++ ticket-manager/Funcionarios.cs
@@ -0,0 +1,11 @@
+﻿class Funcionarios
+{
+    private string nome { get; set; }
+    private string cpf { get; set; }
+
+    public Funcionarios(string nome, string cpf)
+    {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+}

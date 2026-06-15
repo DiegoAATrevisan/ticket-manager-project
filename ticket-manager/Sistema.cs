3674ba162e24f93c87782e2fbf91686aba1a728e
Diego A A Trevisan <augustodiego02@gmail.com>
2026-06-15 19:57:27 -0300

    Atualiza o menu do sistema para remover opções de clientes e reorganizar as opções disponíveis

--- ticket-manager/Sistema.cs
+++ ticket-manager/Sistema.cs
@@ -1,35 +1,35 @@
+using DotNetEnv;
+Env.Load();
 class Sistema
 {
     static void Main(string[] args)
     {
+        SistemaFuncionario sistemaFuncionario = new SistemaFuncionario();
+
         Boolean SysUp = true;
 
         while (SysUp)
         {
             Console.WriteLine("Bem Vindo ao Sistema de Gerenciamento de Tickets!");
             Console.WriteLine("Digite 1 para acessar o menu de funcionários");
-            Console.WriteLine("Digite 2 para acessar o menu de clientes");
-            Console.WriteLine("Digite 3 para acessar o menu de tickets");
-            Console.WriteLine("Digite 4 para acessar o menu de relatórios");
-            Console.WriteLine("Digite 5 para sair do sistema");
+            Console.WriteLine("Digite 2 para acessar o menu de tickets");
+            Console.WriteLine("Digite 3 para acessar o menu de relatórios");
+            Console.WriteLine("Digite 4 para sair do sistema");
 
             int opcao = int.Parse(Console.ReadLine());
 
             switch (opcao)
             {
                 case 1:
-                    MenuFuncionarios.MenuFuncionarios();
+                    sistemaFuncionario.Sistema();
                     break;
                 case 2:
-                    MenuClientes.MenuClientes();
+                    // MenuTickets.MenuTickets();
                     break;
                 case 3:
-                    MenuTickets.MenuTickets();
+                    // MenuRelatorios.MenuRelatorios();
                     break;
                 case 4:
-                    MenuRelatorios.MenuRelatorios();
-                    break;
-                case 5:
                     SysUp = false;
                     Console.WriteLine("Saindo do sistema...");
                     break;

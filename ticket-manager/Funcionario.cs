f279f1a2a8dd1e1f230502183d5312225e493523
Diego A A Trevisan <augustodiego02@gmail.com>
2026-06-15 18:57:14 -0300

    Renomeação da classe para melhor entendimento.

--- ticket-manager/Funcionario.cs
+++ ticket-manager/Funcionario.cs
@@ -1,9 +1,9 @@
-﻿class Funcionarios
+﻿class Funcionario
 {
     private string nome { get; set; }
     private string cpf { get; set; }
 
-    public Funcionarios(string nome, string cpf)
+    public Funcionario(string nome, string cpf)
     {
         this.nome = nome;
         this.cpf = cpf;

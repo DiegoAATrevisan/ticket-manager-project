945cc112ce2d2e3973a6a4e2e8d3a8955b40cbc0
Diego A A Trevisan <augustodiego02@gmail.com>
2026-06-16 00:31:23 -0300

    Refatorar método OnConfiguring para melhorar a legibilidade e garantir a configuração da conexão com o banco de dados

--- ticket-manager/Db.cs
+++ ticket-manager/Db.cs
@@ -1,16 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
 
 public class AppDbContext : DbContext
 {
     public DbSet<Funcionario> funcionarios => Set<Funcionario>();
     public DbSet<Tickets> tickets => Set<Tickets>();
 
-    protected override void OnConfiguring(
-    DbContextOptionsBuilder optionsBuilder)
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        var connectionString =
-            Environment.GetEnvironmentVariable(
-                "POSTGRES_CONNECTION_STRING");
+        var connectionString = Environment.GetEnvironmentVariable("POSTGRES_CONNECTION_STRING");
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("POSTGRES_CONNECTION_STRING não foi configurada no arquivo .env");
+        }
 
         optionsBuilder.UseNpgsql(connectionString);
     }

f61d9f77c05b3f2cea3e635549d810ed042e2c40
Diego A A Trevisan <augustodiego02@gmail.com>
2026-06-16 19:05:12 -0300

    Criação do projeto web para o Ticket Manager.

--- ticket-manager/obj/Debug/net10.0/ticket-manager.AssemblyInfo.cs
+++ ticket-manager/obj/Debug/net10.0/ticket-manager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ticket-manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b837d9cbf2b30c82cc55393b5f8937057cc29c18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+546814d0e9464c9bf0b76e1474a1c332dbbf8dc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ticket-manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ticket-manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

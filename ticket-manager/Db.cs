0c11c0db977db5e191185c8263844e09f493791e
Diego A A Trevisan <augustodiego02@gmail.com>
2026-06-16 15:26:14 -0300

    Adicionar configuração de logging e definir propriedades da entidade Tickets no contexto do banco de dados

--- ticket-manager/Db.cs
+++ ticket-manager/Db.cs
@@ -8,6 +8,7 @@ public class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
         var connectionString = Environment.GetEnvironmentVariable("POSTGRES_CONNECTION_STRING");
 
         if (string.IsNullOrEmpty(connectionString))
@@ -16,5 +17,59 @@ public class AppDbContext : DbContext
         }
 
         optionsBuilder.UseNpgsql(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Funcionario>(entity =>
+        {
+            entity.ToTable("funcionarios");
+
+            entity.HasKey(f => f.Id);
+
+            entity.Property(f => f.Id)
+                .HasColumnName("id");
+
+            entity.Property(f => f.Nome)
+                .HasColumnName("nome")
+                .HasMaxLength(100);
+
+            entity.Property(f => f.Cpf)
+                .HasColumnName("cpf")
+                .HasMaxLength(11);
+
+            entity.Property(f => f.Situacao)
+                .HasColumnName("situacao")
+                .HasDefaultValueSql("'A'")
+                .ValueGeneratedOnAdd();
+
+            entity.Property(f => f.DataCadastro)
+                .HasColumnName("data_cadastro")
+                .ValueGeneratedOnAdd();
+        });
+
+        modelBuilder.Entity<Tickets>(entity =>
+        {
+            entity.ToTable("tickets");
+
+            entity.HasKey(t => t.Id);
+
+            entity.Property(t => t.Id)
+                .HasColumnName("id");
+            entity.Property(t => t.IdFuncionario)
+                .HasColumnName("id_funcionario");
+
+            entity.Property(t => t.Quantidade)
+                .HasColumnName("quantidade");
+
+            entity.Property(t => t.Situacao)
+                .HasColumnName("situacao")
+                .HasDefaultValueSql("'A'")
+                .ValueGeneratedOnAdd();
+
+            entity.Property(t => t.DataCadastro)
+                .HasColumnName("data_cadastro")
+                .ValueGeneratedOnAdd();
+        });
     }
 }
